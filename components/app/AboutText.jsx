23fbbc061718c20b6bc9938b7b8a9c3123f90bbc
Johanna-hub <jyherman@gmail.com>
2020-11-05 20:45:09 +0000

    second lockdown update

--- components/app/AboutText.jsx
+++ components/app/AboutText.jsx
@@ -30,9 +30,10 @@ const About = () => {
   return (
       <Box>
         <AboutContainer>
-        <AboutText>Please follow us on Instagram at <AboutLink href="https://www.instagram.com/brixtonlocal.life/">@brixtonlocal.life</AboutLink> for daily updates on the businesses featured here.</AboutText>
-          <AboutText>Brixton Local.Life kickstarted the <AboutLink href="https://www.lovelocal.life/">LoveLocal.Life</AboutLink> project.  Check out the <AboutLink href="https://www.sydenhamlocal.life/">Sydenham</AboutLink>, <AboutLink href="https://www.lewishamlocal.life/">Lewisham</AboutLink>, <AboutLink href="https://www.wokinghamlocal.life/">Wokingham</AboutLink> and <AboutLink href="https://www.winchmorehilllocal.life/">Winchmore Hill </AboutLink>sites!</AboutText>  
-          <AboutText>We started this website during the Covid-19 Lockdown to connect people with businesses and services still operating in Brixton, and to help businesses move online quickly.</AboutText>
+        <AboutText>We started this website during the first Covid-19 Lockdown to connect people with businesses and services still operating in Brixton, and to help businesses move online quickly.</AboutText>
+        <AboutText>It is a big process to update it for the second Lockdown - so if your business is listed, please do <AboutLink href = "mailto: brixtonlocal@gmail.com">email</AboutLink> us with a few sentences that we can post for you.</AboutText>
+        <AboutText>Follow us on Instagram at <AboutLink href="https://www.instagram.com/brixtonlocal.life/">@brixtonlocal.life</AboutLink> for daily updates on the businesses featured here.</AboutText>
+          <AboutText>Brixton Local.Life kickstarted the <AboutLink href="https://www.lovelocal.life/">LoveLocal.Life</AboutLink> project.</AboutText>  
           <AboutText>We thought if we could support local people and businesses in this small way, we might just come out of this stronger and more aware of what’s on offer in our amazing local area, and how they continued to serve us, even under the most difficult circumstances.</AboutText>
           <AboutText>We’ve defined Brixton pretty loosely, taking in bits of Herne Hill, Stockwell, Clapham and Streatham, depending on delivery areas, and so there’s a range of services.</AboutText>
           <AboutText>Obviously this is a fast-changing situation, so it’s always worth double checking websites and social media for the most up to date information.</AboutText>
@@ -41,7 +42,6 @@ const About = () => {
           <AboutText>To set up a similar project in your area please go to the <AboutLink href="https://www.lovelocal.life/">LoveLocal.Life</AboutLink> website.</AboutText>
           <AboutText><AboutLink href="https://www.lovelocal.life/">LoveLocal.Life</AboutLink> supports local communities during the COVID-19 lockdown by helping them easily set up websites that spread the word about independent businesses and services in their area.  
           This includes businesses that are still operational, new delivery services and other activities, such as community groups and local support.</AboutText>
-          <AboutText>To get in contact please  <AboutLink href = "mailto: brixtonlocal@gmail.com">email</AboutLink> us.</AboutText>
         </AboutContainer>
       </Box>
   )
